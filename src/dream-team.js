dbf0623daebbc4e4b21a1ce9aaf41edfcde0c826
TheBeOff <60623496+TheBeOff@users.noreply.github.com>
2020-03-22 19:49:59 +0300

    fix: fix code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,5 @@
 module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) return false;
   members = members.filter(item => typeof(item) === 'string');
-  return members.map(elem => elem.toUpperCase() .trim().slice(0,1)).sort().join('')
+  return members.map(elem => elem.toUpperCase().trim().slice(0,1)).sort().join('')
 };
