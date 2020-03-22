c20106f67f76fa430290a7c60b80d2f3dd9865dd
TheBeOff <60623496+TheBeOff@users.noreply.github.com>
2020-03-22 19:47:55 +0300

    feat: add code in dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,5 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  members = members.filter(item => typeof(item) === 'string');
+  return members.map(elem => elem.toUpperCase() .trim().slice(0,1)).sort().join('')
 };
